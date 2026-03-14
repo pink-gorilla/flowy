b823161448594aadf98848ac25b7e24ebb9e29ae
awb99 <awb99@gmail.com>
2026-03-14 18:47:04 -0500

    flow working with clojure 1.12

--- demo-webly/src/demo/handler.clj
+++ demo-webly/src/demo/handler.clj
@@ -1,7 +1,5 @@
 (ns demo.handler)
 
-
-
 (defn test-handler [req]
   {:status 200 :body "<html> <body> hello, test </body> </html>"})
   
--- demo-webly/src/demo/wapp.cljs
+++ demo-webly/src/demo/wapp.cljs
@@ -1,22 +1,14 @@
 (ns demo.wapp
   (:require
-   [webly.spa.env :refer [get-resource-path]]))
+   [shadowx.core :refer [get-resource-path]]))
 
 (defn link [url text]
   [:a {:href url} [:span {:style {:padding "2px"}} text]])
 
-(defn nav []
-  [:div.bg-blue-300
-   [link "/#" "goto options"]
-   [link "/#/controls" "goto controls"]
-   [link "/#/quanta" "goto quanta"]
-   [link "/#/clj" "goto clj-options"]])
 
 (defn wrap-app [page match]
   [:div
-   ;[nav]
    [page match]])
 
 (def routes
-  [["/" {:name 'demo.reagent/app}]
-   ])
+  [["/" {:name 'demo.reagent/app}]])
